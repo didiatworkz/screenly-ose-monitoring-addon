df9d2af1f2045a5834b563e960830762bf284a36
didiatworkz <skyn3t.syst3m@gmail.com>
2019-10-02 09:49:06 +0200

    fix name set

--- roles/add-on/files/screenshot.sh
+++ roles/add-on/files/screenshot.sh
@@ -3,7 +3,7 @@
 cp /var/www/html/addon/loading.png /var/www/html/addon/screen/screenshot.png
 sleep 40;
 while true; do
-   raspi2png /var/www/html/addon/screen/screenshot_tmp.png
+   raspi2png --pngname /var/www/html/addon/screen/screenshot_tmp.png
    cp -f /var/www/html/addon/screen/screenshot_tmp.png /var/www/html/addon/screen/screenshot.png
    sleep 2;
 done
