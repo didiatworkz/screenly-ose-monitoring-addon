86b5ca743405afd5fbb04ba79849cb09c46d64bc
didiatworkz <skyn3t.syst3m@gmail.com>
2019-10-02 09:16:57 +0200

    implement raspi2png

--- roles/add-on/files/screenshot.sh
+++ roles/add-on/files/screenshot.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # Screenly OSE Monitoring Add-on
 cp /var/www/html/addon/loading.png /var/www/html/addon/screen/screenshot.png
-sleep 60;
+sleep 40;
 while true; do
-   DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/ xwd -root > /var/www/html/addon/screen/screenshot.xwd
-   convert /var/www/html/addon/screen/screenshot.xwd /var/www/html/addon/screen/screenshot_tmp.png
+   raspi2png /var/www/html/addon/screen/screenshot_tmp.png
    cp -f /var/www/html/addon/screen/screenshot_tmp.png /var/www/html/addon/screen/screenshot.png
-   sleep 3;
+   sleep 2;
 done
 exit
