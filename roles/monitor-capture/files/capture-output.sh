46abd35b07240b6ef792b43f58fe22330cffcd60
Philipp Dyhring <skyn3t.syst3m@gmail.com>
2020-07-06 22:13:28 +0200

    add capture restart service

--- roles/monitor-capture/files/capture-output.sh
+++ roles/monitor-capture/files/capture-output.sh
@@ -15,18 +15,7 @@
 #		     Capture Output Version 2.0
 #	________________________________________
 
+raspi2png --pngname /home/pi/soma/monitor-output/tmp/screenshot_tmp.png  || cp -f /home/pi/soma/monitor-output/error.png /home/pi/soma/monitor-output/tmp/screenshot_tmp.png
+cp -f /home/pi/soma/monitor-output/tmp/screenshot_tmp.png /home/pi/soma/monitor-output/tmp/screenshot.png
 
-_VERSION="2"
-_WAIT_TIME="20"
-
-echo "Screenshot Add-on for Screenly OSE Monitoring"
-cp /home/pi/soma/monitor-output/booting.png /home/pi/soma/monitor-output/tmp/screenshot.png
-echo "wait $_WAIT_TIME sec..."
-sleep "$_WAIT_TIME";
-echo "start screenshots..."
-while true; do
-   raspi2png --pngname /home/pi/soma/monitor-output/tmp/screenshot_tmp.png  || cp -f /home/pi/soma/monitor-output/error.png /home/pi/soma/monitor-output/tmp/screenshot_tmp.png
-   cp -f /home/pi/soma/monitor-output/tmp/screenshot_tmp.png /home/pi/soma/monitor-output/tmp/screenshot.png
-   sleep 3;
-done
 exit 0
