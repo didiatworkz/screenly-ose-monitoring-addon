e3e5e18e4f13d27aaceda47c50d1f605d3edbc74
Philipp Dyhring <skyn3t.syst3m@gmail.com>
2020-10-31 23:45:30 +0100

    remove tee

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ sudo rm -rf /tmp/soma
 sudo git clone --branch $_BRANCH https://github.com/didiatworkz/screenly-ose-monitoring-addon.git /tmp/soma
 cd /tmp/soma
 touch /home/$_EXECUTED_USER/soma_install.log
-sudo -E ansible-playbook addon.yml | tee /home/$_EXECUTED_USER/soma_install.log
+sudo -E ansible-playbook addon.yml
 
 header
 echo "Screenly OSE Monitor addon successfuly installed"
