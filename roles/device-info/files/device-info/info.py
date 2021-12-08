bbed8cc54fcfbf1d8c7bfe37bf15754b21b1e89e
Philipp Hörauf <skyn3t.syst3m@gmail.com>
2021-12-08 07:48:59 +0100

    Revert "change platform endpoint"
    This reverts commit 62fbb4d5d962094247348ea7cbe28c37aaed933e.

--- roles/device-info/files/device-info/info.py
+++ roles/device-info/files/device-info/info.py
@@ -21,7 +21,7 @@ import distro
 from subprocess import check_output
 from flask import Flask, url_for
 
-_VERSION='1.3'
+_VERSION='1.2'
 _HEADER='Screenly OSE Monitoring Add-On - Device Info V' + _VERSION
 
 app = Flask('__name__')
@@ -93,13 +93,9 @@ def memory_total():
     total = round(memory.total/1024.0/1024.0,1)
     return str(total)
 
-@app.route('/platform_name')
+@app.route('/platform')
 def platform():
-    return str(distro.name())
-
-@app.route('/platform_version')
-def platform():
-    return str(distro.codename())
+    return str(distro.linux_distribution())
 
 @app.route('/process')
 def running_process_list():
